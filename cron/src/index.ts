f0a244815d30039daf74dc6eb7263f15081d86fa
kurdy <gitlab@kurdy.ch>
2023-11-05 15:32:48 +0100

    version 0.1.1

--- cron/src/index.ts
+++ cron/src/index.ts
@@ -29,21 +29,21 @@ export default {
 		// Source and informations https://www.rates.bazg.admin.ch/home
 		// https://www.backend-rates.bazg.admin.ch/api/xmldaily?d=20230927&locale=en
 		const now = new Date();
-		//const keys = [...(await env.KV_CURRENCIES_RATES.list()).keys];
-		//console.log(`List (${keys.length}): ${JSON.stringify(keys)}`);
-		//await env.KV_CURRENCIES_RATES.delete('key_last_request');
-		const date = `${now.getFullYear()}${(now.getMonth() +1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}`;
-		const response = await fetch(`${env.RATES_REQUEST_URL}&d=${date}`);
+		const reqDate = `${now.getFullYear()}${(now.getMonth() +1).toString().padStart(2, '0')}${now.getDate().toString().padStart(2, '0')}`;
+		const request = `${env.RATES_REQUEST_URL}&d=${reqDate}`;
+		console.log(`request=${request}`);
+		const response = await fetch(request);
 		if(response.ok){
 			const content = await response.text();
 			if (!env.DISABLE_HASH) {
 				const hash = h32( content, 0 ).toString();
-				const previousHash = await env.KV_CURRENCIES_RATES.get('key_hash');
+				const previousHash = await env.KV_CURRENCIES_RATES.get('KEY_HASH');
 				if (hash !== previousHash) {
 					console.log('Update KV store');
 					await parseAndStore(content,env);	
-					await env.KV_CURRENCIES_RATES.put('k',hash,{
-						metadata: { updated: now.toUTCString() },
+					await env.KV_CURRENCIES_RATES.put('KEY_HASH',hash,{
+						metadata: { updated: now.toUTCString(),
+						expirationTtl: 86400},
 					});
 				} else {
 					console.log(`KV store is up-to-date. Hash: ${previousHash}`);
@@ -63,40 +63,56 @@ async function parseAndStore(data:string,env: Env): Promise<void> {
 
 	const parser = new XMLParser();
 	const jObj = parser.parse(data);
-
 	if (jObj && jObj.wechselkurse && jObj.wechselkurse.devise) {
-		const rateDateArray : string[]= jObj.wechselkurse.datum.split('.');
-		const rateHourArray : string[]= jObj.wechselkurse.zeit.split(':');
-		const validityArray : string[]= jObj.wechselkurse.gueltigkeit.split('.');
+		const rateDateArray : string[]= jObj.wechselkurse.datum.split('.');	// <datum>03.11.2023</datum>
+		const rateHourArray : string[]= jObj.wechselkurse.zeit.split(':'); 	// <zeit>03:05:01</zeit>
 		const rateDt = DateTime.fromObject({
 			year: Number.parseInt(rateDateArray[2]),
 			month: Number.parseInt(rateDateArray[1]),
 			day: Number.parseInt(rateDateArray[0]),
 			hour: Number.parseInt(rateHourArray[0]),
 			minute: Number.parseInt(rateHourArray[1]),
-			second: Number.parseInt(rateHourArray[0])}, { zone: 'Europe/Zurich' });
-		const validityDt = DateTime.fromObject({
-			year: Number.parseInt(validityArray[2]),
-			month: Number.parseInt(validityArray[1]),
-			day: Number.parseInt(validityArray[0]),
-			hour: 23,
-			minute: 59,
-		  	second: 59 }, { zone: 'Europe/Zurich' });
+			second: Number.parseInt(rateHourArray[0])}, 
+			{ zone: 'Europe/Zurich' });
+		const validityDates : DateTime[]= jObj.wechselkurse.gueltigkeit.split(',').map((i:string) =>{
+				const validityArray : string[]= i.split('.');
+				return DateTime.fromObject({
+					year: Number.parseInt(validityArray[2]),
+					month: Number.parseInt(validityArray[1]),
+					day: Number.parseInt(validityArray[0]),
+					hour: 23,
+					minute: 59,
+					second: 59}, 
+					{ zone: 'Europe/Zurich' });
+			}); // weekends and public holidays there could be an array <gueltigkeit>04.11.2023,05.11.2023,06.11.2023</gueltigkeit>
+		const validityDt = validityDates.reduce((a,c)=>{
+			return a.valueOf() >= c.valueOf() ? a : c;
+		});
 		const currenciesRates : CurrencyRate[] = [...jObj.wechselkurse.devise.map((currency:any)=>toCurrencyRate(currency,rateDt,validityDt))]; 
 		console.log(`${currenciesRates.length} currencies converted.`);
 		for (const c of currenciesRates) {
-			await env.KV_CURRENCIES_RATES.put(`key_${c.code}`, `${JSON.stringify(c)}`,{
-				expiration: validityDt.plus({hours:12}).valueOf() / 1000
+			await env.KV_CURRENCIES_RATES.put(`KEY_C_${c.code}`.toUpperCase(), `${JSON.stringify(c)}`,{
+				expiration: validityDt.plus({hours:6}).valueOf() / 1000
 			});
 		}
 		//console.log(`${JSON.stringify((await env.KV_CURRENCIES_RATES.list()).keys)}`);
-		console.log(await env.KV_CURRENCIES_RATES.get('key_EUR'));
+		console.log(await env.KV_CURRENCIES_RATES.get('KEY_EUR'));
 	} else {
 		console.error(`Invalid data. ${data}`)
 	}
 }
 
 function toCurrencyRate (currency : any,rateDt:DateTime,validityDt:DateTime) : CurrencyRate {
+	/**
+	 	<devise code="eur">
+			<land_de>Europäische Währungsunion</land_de>
+			<land_fr>Union monétaire européenne</land_fr>
+			<land_it>Unione Monetaria Europea</land_it>
+			<land_en>Euro Member</land_en>
+			<waehrung>1 EUR</waehrung>
+			<kurs>0.97213</kurs>
+		</devise>
+	*/
 	const i18nTexts : CurrencyI18n[] = [];
 			i18nTexts.push(
 				{
